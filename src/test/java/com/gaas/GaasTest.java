c64310f13ebd9422cbcd7def2d32e51148c5320d
Robb Tran <bobtran@usc.edu>
2022-02-09 05:19:26 +0000

    Initial commit

---
+++ src/test/java/com/gaas/GaasTest.java
@@ -0,0 +1,21 @@
+package com.gaas;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class GaasTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
